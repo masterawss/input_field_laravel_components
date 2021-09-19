3f6f9e6d0613ea7791c9425b88e82e0bfc3826bd
Anthony Will Solsol Soplin <masterawss@gmail.com>
2021-09-19 09:37:18 -0500

    asd

--- src/views/components/simple-list.blade.php
+++ src/views/components/simple-list.blade.php
@@ -1,6 +1,6 @@
 <li class="media mb-1">
     <div class="media-body">
-        <h6 class="font-16 mb-1">{{$titulo}}</h6>
-        <p>{{$descripcion ?? '-'}}</p>
+        <h6 class="font-16 mb-1">{{$label}}</h6>
+        <p>{{$value ?? '-'}}</p>
     </div>
 </li>
